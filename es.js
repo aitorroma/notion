7f073a7fbbac46d05e93d118570adcd4f5dc3fab
Aitor Roma <71042576+aitorroma@users.noreply.github.com>
2021-06-21 22:51:58 +0200

    Create es.js

---
+++ es.js
@@ -0,0 +1,20 @@
+window.CUSDIS_LOCALE = {
+    powered_by: '',
+    post_comment: 'Comentar',
+    loading: 'Cargando',
+  
+    // comment
+    email: 'Correo electrónico',
+    nickname: 'Nombre',
+    reply_placeholder: 'Responder...',
+    reply_btn: 'Responder',
+    sending: 'Enviando...',
+  
+    // reply
+    mod_badge: 'MOD',
+    content_is_required: 'El contenido es obligatorio',
+    nickname_is_required: 'El nombre es obligatorio',
+  
+    comment_has_been_sent:
+      'Tu comentario ha sido enviado. Por favor, espera su aprobación.',
+  }
